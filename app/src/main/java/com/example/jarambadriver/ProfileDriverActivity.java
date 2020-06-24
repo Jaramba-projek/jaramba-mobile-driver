4c50f287538c310e803e5e8376200b4be35bab42
bagasbest <bagasbest80@gmail.com>
2020-06-24 16:16:55 +0700

    update dynamic background

--- app/src/main/java/com/example/jarambadriver/ProfileDriverActivity.java
+++ app/src/main/java/com/example/jarambadriver/ProfileDriverActivity.java
@@ -3,17 +3,24 @@ package com.example.jarambadriver;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Calendar;
 
 public class ProfileDriverActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
+    ImageView greetImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,8 @@ public class ProfileDriverActivity extends AppCompatActivity {
 
         bottomNavigationView = findViewById(R.id.menu_navigasi);
         bottomNavigationView.setSelectedItemId(R.id.profile);
+
+        greetImg = findViewById(R.id.greeting_img_profile);
 
         setOnNavigationSelectedListener();
 
@@ -47,6 +56,22 @@ public class ProfileDriverActivity extends AppCompatActivity {
     }
 
     public void logout(View view) {
+
+    }
+
+
+    @SuppressLint("SetTextI18n")
+    private void greeting() {
+        Calendar calendar = Calendar.getInstance();
+        int timeOfDay = calendar.get(Calendar.HOUR_OF_DAY);
+
+        if (timeOfDay >= 0 && timeOfDay < 18) {
+            greetImg.setImageResource(R.drawable.img_default_half_morning);
+            Glide.with(ProfileDriverActivity.this).load(R.drawable.img_default_half_morning).into(greetImg);
+        }else if (timeOfDay >= 18 && timeOfDay < 24) {
+            Glide.with(ProfileDriverActivity.this).load(R.drawable.img_default_half_night).into(greetImg);
+            greetImg.setImageResource(R.drawable.malamhari);
+        }
 
     }
 
