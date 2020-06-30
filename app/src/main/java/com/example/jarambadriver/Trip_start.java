aa7bf5a36d5d2862a2c5f4902ef8fd7c97e13798
bagasbest <bagasbest80@gmail.com>
2020-06-30 10:56:01 +0700

    update start trip dan finish trip

--- app/src/main/java/com/example/jarambadriver/Trip_start.java
+++ app/src/main/java/com/example/jarambadriver/Trip_start.java
@@ -1,9 +1,12 @@
 package com.example.jarambadriver;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -19,22 +22,34 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Calendar;
+import java.util.HashMap;
 
 public class Trip_start extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
+    DatabaseReference databaseReference;
+
+
     ImageView greetImg;
     TextView greetText;
-    Button btnstart;
     Spinner trayek, noKendaraan;
+    Button btnStart, btnFinish;
+
+    ProgressDialog progressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip_start);
 
-       greetImg = findViewById(R.id.greeting_img);
+//        databaseReference = FirebaseDatabase.getInstance().getReference("driver_trips");
+        progressDialog = new ProgressDialog(this);
+
+        greetImg = findViewById(R.id.greeting_img);
         greetText = findViewById(R.id.greeting_text);
 
         trayek = findViewById(R.id.btn_trayek);
@@ -50,13 +65,6 @@ public class Trip_start extends AppCompatActivity implements AdapterView.OnItemS
 
         greeting();
 
-        btnstart = findViewById(R.id.btn_start);
-        btnstart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),Trip_finish.class));
-            }
-        });
 
         BottomNavigationView bottomNavigationView =  findViewById(R.id.menu_navigasi);
         bottomNavigationView.setSelectedItemId(R.id.trip);
@@ -86,29 +94,30 @@ public class Trip_start extends AppCompatActivity implements AdapterView.OnItemS
 
     }
 
+
     @SuppressLint("SetTextI18n")
     private void greeting() {
-        Calendar calendar = Calendar.getInstance();
-        int timeOfDay = calendar.get(Calendar.HOUR_OF_DAY);
-
-        if (timeOfDay >= 0 && timeOfDay < 18) {
-            if(timeOfDay > 3 && timeOfDay <12 ) {
-                greetText.setText("Good Morning");
-            } else if(timeOfDay >=12) {
-                greetText.setText("Good Afternoon");
-            }
-            greetImg.setImageResource(R.drawable.img_default_half_morning);
-            Glide.with(Trip_start.this).load(R.drawable.img_default_half_morning).into(greetImg);
-        }else if (timeOfDay >= 18 && timeOfDay < 24) {
-            if(timeOfDay < 21 ) {
-                greetText.setText("Good Evening");
-            } else if(timeOfDay > 21) {
-                greetText.setText("Good Night");
-            }
-            greetText.setTextColor(Color.WHITE);
-            Glide.with(Trip_start.this).load(R.drawable.img_default_half_night).into(greetImg);
-            greetImg.setImageResource(R.drawable.malamhari);
-        }
+//        Calendar calendar = Calendar.getInstance();
+//        int timeOfDay = calendar.get(Calendar.HOUR_OF_DAY);
+//
+//        if (timeOfDay >= 0 && timeOfDay < 18) {
+//            if(timeOfDay > 3 && timeOfDay <12 ) {
+//                greetText.setText("Good Morning");
+//            } else if(timeOfDay >=12) {
+//                greetText.setText("Good Afternoon");
+//            }
+//            greetImg.setImageResource(R.drawable.img_default_half_morning);
+//            Glide.with(Trip_start.this).load(R.drawable.img_default_half_morning).into(greetImg);
+//        }else if (timeOfDay >= 18 && timeOfDay < 24) {
+//            if(timeOfDay < 21 ) {
+//                greetText.setText("Good Evening");
+//            } else if(timeOfDay > 21) {
+//                greetText.setText("Good Night");
+//            }
+//            greetText.setTextColor(Color.WHITE);
+//            Glide.with(Trip_start.this).load(R.drawable.img_default_half_night).into(greetImg);
+//            greetImg.setImageResource(R.drawable.malamhari);
+//        }
 
     }
 
@@ -120,5 +129,153 @@ public class Trip_start extends AppCompatActivity implements AdapterView.OnItemS
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    public void startTripDriver(View view) {
+        //create alert dialog before start trip
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("perintah memulai perjalanan");
+
+        builder.setIcon(R.drawable.ic_warning_black_24dp);
+        builder.setMessage("Anda yakin ingin memulai perjalanan ? ");
+        builder.setCancelable(false);
+
+        builder.setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //mulai perjalanan
+                startTrip();
+            }
+        });
+
+        builder.setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+    }
+
+    private void startTrip() {
+        progressDialog();
+        Calendar calendar = Calendar.getInstance();
+        int timeOfDay = calendar.get(Calendar.HOUR_OF_DAY);
+
+        trayek = findViewById(R.id.btn_trayek);
+        noKendaraan = findViewById(R.id.btn_plat);
+        btnStart = findViewById(R.id.btn_start_trip);
+        btnFinish = findViewById(R.id.btn_finish_trip);
+
+
+        btnStart.setVisibility(View.GONE);
+        trayek.setEnabled(false);
+        noKendaraan.setEnabled(false);
+
+        String email = "bagasganteng88@gmail.com";
+        String trayek_pilihan = trayek.getSelectedItem().toString().trim();
+        String nomor_kendaraan_pilihan = noKendaraan.getSelectedItem().toString().trim();
+
+//        HashMap<Object, String> hashMap = new HashMap<>();
+//        hashMap.put("email", email);
+//        hashMap.put("trayek", trayek_pilihan);
+//        hashMap.put("Nama_Lengkap", nomor_kendaraan_pilihan);
+
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference reference = database.getReference("driver_trips");
+//        reference.child(email).setValue(hashMap);
+
+
+        if(timeOfDay > 18) {
+            btnStart.setVisibility(View.VISIBLE);
+            trayek.setEnabled(true);
+            noKendaraan.setEnabled(true);
+        }
+
+        progressDialog.dismiss();
+        Toast.makeText(this, email + "\n" + trayek_pilihan + "\n" + nomor_kendaraan_pilihan, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Selamat memulai perjalanan, jangan lupa berdoa", Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void progressDialog() {
+        progressDialog.show();
+        progressDialog.setContentView(R.layout.progress_dialog);
+        progressDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+    }
+
+    public void finishTripDriver(View view) {
+        //create alert dialog finish trip driver
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("perintah menyelesaikan perjalanan");
+
+        builder.setIcon(R.drawable.ic_warning_black_24dp);
+        builder.setMessage("Anda yakin ingin menyelesaikan perjalanan ? ");
+        builder.setCancelable(false);
+
+        builder.setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //menyelesaikan perjalanan
+                finishTrip();
+
+            }
+        });
+
+        builder.setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+
+
+    }
+
+    private void finishTrip() {
+        trayek = findViewById(R.id.btn_trayek);
+        noKendaraan = findViewById(R.id.btn_plat);
+        btnStart = findViewById(R.id.btn_start_trip);
+        btnFinish = findViewById(R.id.btn_finish_trip);
+
+        btnStart.setVisibility(View.VISIBLE);
+        trayek.setEnabled(true);
+        noKendaraan.setEnabled(true);
+
+        Toast.makeText(Trip_start.this, "Anda menyelesaikan perjalanan", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Konfirmasi keluar aplikasi");
+        builder.setIcon(R.drawable.ic_exit_to_app_black_24dp);
+        builder.setMessage("Anda yakin ingin keluar aplikasi ? ");
+        builder.setCancelable(false);
+
+        builder.setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finishAndRemoveTask();
+                finish();
+            }
+        });
+
+        builder.setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
